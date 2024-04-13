a0f387bcc82b184aa72cfd76c5a570c3f4d009cb
El <mattianc@hotmail.it>
2024-04-13 02:00:54 +0200

    clean code

--- map.js
+++ map.js
@@ -1,75 +1,173 @@
+var geoJ = 'assets/features.geojson';
+var feature;
 map.on('load', () => {
-    map.loadImage('assets/mapbox-icon.png', 
+    map.loadImage('assets/pin.png', 
     (error, image) => {
         if (error) throw error;
         map.addImage('pin', image);
+    });
+    map.loadImage('assets/pinD.png', 
+    (error, image) => {
+        if (error) throw error;
+        map.addImage('pinD', image);
+    });
+    map.loadImage('assets/pinSc.png', 
+    (error, image) => {
+        if (error) throw error;
+        map.addImage('pinSc', image);
+    });
+    map.loadImage('assets/pinSe.png', 
+    (error, image) => {
+        if (error) throw error;
+        map.addImage('pinSe', image);
+    });
+       
+
+        // const geojson = {
+        //   "features": [
+        //     {
+        //       "type": "Feature",
+        //       "properties": {
+        //         "event": "Bagno diurno",
+        //         "startDate": "15/04/2024",
+        //         "startHour": "",
+        //         "endDate": "21/04/2024",
+        //         "gMapsLink": "https://www.google.com/maps/search/?api=1&query=Via+giacosa+30,+Milano",
+        //         "regLink": "",
+        //         "type": "Location",
+        //         "description": ""
+        //       },
+        //       "geometry": {
+        //         "coordinates": [
+        //           9.221438,
+        //           45.494965
+        //         ],
+        //         "type": "Point"
+        //       },
+        //       "id": "0a18449b5966c833d7e9e5d708c0f615"
+        //     },
+        //     {
+        //       "type": "Feature",
+        //       "properties": {
+        //         "event": "ALESSI",
+        //         "startDate": "16/04/2024",
+        //         "startHour": "10:00",
+        //         "endDate": "21/04/2024",
+        //         "gMapsLink": "https://www.google.com/maps/search/?api=1&query=Palazzo+Borromeo+d'Adda,+Via+Alessandro+Manzoni,+41,+20121+Milano+MI,+Italia",
+        //         "regLink": "",
+        //         "type": "Design",
+        //         "description": ""
+        //       },
+        //       "geometry": {
+        //         "coordinates": [
+        //           9.194015,
+        //           45.471326
+        //         ],
+        //         "type": "Point"
+        //       },
+        //       "id": "0b5377b1d6455f45d6c1a6977d80b4a1"
+        //     }
+        //   ]
+        // }
+        
+        $.getJSON(geoJ, function(events){
+
+          for (feature of events.features) {
+            const type = feature.properties.type;
+            const layerID = `${type}`;
+            
+            // console.log(feature)
+            if (!map.getLayer(layerID)) {
+              map.addLayer({
+                id: layerID,
+                type: 'symbol',
+                source: 'events',
+                layout: {
+                  'icon-image': [
+                        'match',
+                        ['get', 'type'],
+                        'Scrocco', 'pinSc',
+                        'Design', 'pinD',
+                        'Serata', 'pinSe',
+                        'pin'
+                      ],
+                  'icon-size': 0.2,
+                  'icon-allow-overlap': true
+                 },
+                filter: ['==', ['get', 'type'], type],
+              });
+            }
+          }
+        }); // getJSON
 
         map.addSource('events', {
-            type: 'geojson',
-            // Use a URL for the value for the `data` property.
-            data: 'assets/features.geojson',
-            cluster: true,
-            clusterRadius: 50,
-            clusterMaxZoom: 11,
+                    type: 'geojson',
+                    // Use a URL for the value for the `data` property.
+                    data: geoJ,
+                    cluster: false,
+                    clusterRadius: 50,
+                    clusterMaxZoom: 11,
         });
 
+       
+    
         // MARKERS
-        map.addLayer({
-            'id': 'events',
-            'type': 'symbol',
-            'source': 'events',
-            'layout': {
-                'icon-image': 'pin',
-                'icon-size': 0.2,
-                'icon-allow-overlap': true,
-            }
-        });
+    //     map.addLayer({
+    //         'id': 'events',
+    //         'type': 'symbol',
+    //         'source': 'events',
+    //         'layout': {
+    //             'icon-image': 'pin',
+    //             'icon-size': 0.2,
+    //             'icon-allow-overlap': true,
+    //         }
+    //     });
 
-        // CLUSTERS
-        map.addLayer({
-            id: 'clusters',
-            type: 'circle',
-            source: 'events',
-            filter: ['has', 'point_count'],
-            paint: {
-                // Use step expressions (https://docs.mapbox.com/style-spec/reference/expressions/#step)
-                'circle-color': [
-                    'step',
-                    ['get', 'point_count'],
-                    '#fdffb8',
-                    5,
-                    '#F9FD60',
-                    15,
-                    '#ffbf47'
+    //     // CLUSTERS
+    //     map.addLayer({
+    //         id: 'clusters',
+    //         type: 'circle',
+    //         source: 'events',
+    //         filter: ['has', 'point_count'],
+    //         paint: {
+    //             // Use step expressions (https://docs.mapbox.com/style-spec/reference/expressions/#step)
+    //             'circle-color': [
+    //                 'step',
+    //                 ['get', 'point_count'],
+    //                 '#fdffb8',
+    //                 5,
+    //                 '#F9FD60',
+    //                 15,
+    //                 '#ffbf47'
                     
-                ],
-                'circle-radius': [
-                    'step',
-                    ['get', 'point_count'],
-                    20,
-                    5,
-                    30,
-                    25,
-                    40
-                ]
-            }
-        });
+    //             ],
+    //             'circle-radius': [
+    //                 'step',
+    //                 ['get', 'point_count'],
+    //                 20,
+    //                 5,
+    //                 30,
+    //                 25,
+    //                 40
+    //             ]
+    //         }
+    //     });
 
-        // CLUSTERS COUNT
-        map.addLayer({
-            id: 'cluster-count',
-            type: 'symbol',
-            source: 'events',
-            // filter: ['has', 'point_count'],
-            layout: {
-                'text-field': ['get', 'point_count_abbreviated'],
-                'text-size': 12
-            },
-            paint: {
-                "text-color": "#000000"
-            }
-        });
-    });
+    //     // CLUSTERS COUNT
+    //     map.addLayer({
+    //         id: 'cluster-count',
+    //         type: 'symbol',
+    //         source: 'events',
+    //         // filter: ['has', 'point_count'],
+    //         layout: {
+    //             'text-field': ['get', 'point_count_abbreviated'],
+    //             'text-size': 12
+    //         },
+    //         paint: {
+    //             "text-color": "#000000"
+    //         }
+    //     });
+    // });
 
 });
 
@@ -92,8 +190,74 @@ map.on('click', 'clusters', (e) => {
     );
 });
 
+// for (e of e.features[0]) {
+   
+//   el.addEventListener('click', (e) => {
+//       console.log('aa')
+//   });
+// }
+function poppinUp(e){
+  const coordinates = e.features[0].geometry.coordinates.slice();
+  const type = e.features[0].properties.type;
+  const event = e.features[0].properties.event;
+  const description = e.features[0].properties.description;
+  const startDate = e.features[0].properties.startDate;
+  const endDate = e.features[0].properties.endDate;
+  const gMapsLink = e.features[0].properties.gMapsLink;
+  const regLink = e.features[0].properties.regLink;
 
-map.on('click', 'events', (e) => {
+    // Ensure that if the map is zoomed out such that
+    // multiple copies of the feature are visible, the
+    // popup appears over the copy being pointed to.
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    }
+
+    let popup = new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setHTML(` <div class="popUp">
+            <div class="popUp-title">${type}</div>
+                <div class="waiting-time-big sans-bold">
+                        ${event}
+                        <div>
+                        ${description}
+                        </div>
+                    <div class="next-spot sans-regular"> Inizio <span class="sans-bold"> ${startDate} </span></div>
+                    <div class="next-spot sans-regular"> Fine <span class="sans-bold"> ${endDate} </span></div>
+                    <div class="comparison-data">
+                        
+                    </div>
+                </div>
+
+                <div class="popUp-footer flex-display-center-sb">
+                    <a href="${gMapsLink}" class="stream-button list-btn btn-text">Google maps</a>
+                    <a href="${regLink}" class="btn-text sans-bold underlined cancello">Registrati</a>
+                </div>
+            </div>`
+        )
+        .setMaxWidth("80%")
+        .addTo(map);
+
+    map.flyTo({
+        center: e.features[0].geometry.coordinates
+    });
+
+    // LINK HIDDEN IF VALUE NOT PRESENT
+    console.log("link is:" + regLink)
+    if(!regLink){
+        console.log("no link present")
+        $(".cancello").hide();
+    }
+    else{
+        console.log('link exists')
+    }
+  console.log('idea giusta')
+}
+map.on('click', 'Design', (e) => 
+  poppinUp()
+);
+
+map.on('click', 'Scrocco', (e) => {
     const coordinates = e.features[0].geometry.coordinates.slice();
     const type = e.features[0].properties.type;
     const event = e.features[0].properties.event;
@@ -107,12 +271,7 @@ map.on('click', 'events', (e) => {
     //     console.log('aaa')
     // });
 
-    // for (e of geojson.features) {
-   
-    //     el.addEventListener('click', (e) => {
-    //         console.log('aa')
-    //     });
-    // }
+    
     
     // const tsunami =
     //     e.features[0].properties.tsunami === 1 ? 'yes' : 'no';
